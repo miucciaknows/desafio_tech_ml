fc11b482c0498a94f5e6bea43326b29f205f7a1c
Nathalia Trazzi <81270839+miucciaknows@users.noreply.github.com>
2024-03-13 13:33:12 -0300

    Update request_model.py
    Adicionando comentários para a classe AgendamentoRequest

--- source/rest_api/models/request_model.py
+++ source/rest_api/models/request_model.py
@@ -1,6 +1,14 @@
+#Importando a classe BaseModel da biblioteca pydantic
+
 from pydantic import BaseModel
 
 class AgendamentoRequest(BaseModel):
+    """
+    Modelo de dados para representar uma solicitação de agendamento de envio de comunicação.
+    """
+    # Data e hora para o envio da comunicação (string)
     data_hora_envio: str
+    # Destinatário da comunicação (string)
     destinatario: str
+    # Mensagem a ser entregue (string)
     mensagem: str
