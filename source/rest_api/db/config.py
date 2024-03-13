709437306a9d177db0c240a10e630c83525bee61
Nathalia Trazzi <81270839+miucciaknows@users.noreply.github.com>
2024-03-13 13:36:33 -0300

    Update config.py
    Adicionando comentarios na classe DatabaseConfig

--- source/rest_api/db/config.py
+++ source/rest_api/db/config.py
@@ -1,14 +1,26 @@
+# Importando biblioteca o psycopg2 para trabalhar com PostgreSQL
 import psycopg2
+# Importando os para acessar variáveis de ambiente
 import os
+# Importando load_dotenv do módulo dotenv para carregar variáveis de ambiente de um arquivo .env
 from dotenv import load_dotenv
-
+ # Importando a classe InitialDatabaseConfig do módulo db.initial_config
 from db.initial_config import InitialDatabaseConfig
 
 class DatabaseConfig:
+    """
+    Configurações relacionadas ao banco de dados.
+    """
     def __init__(self):
+        """
+        Metodo para a classe carregar as variáveis de ambiente.
+        """
         load_dotenv()
 
     def data_base_connection(self):
+        """
+        Criando e retornando uma conexão com o banco de dados.
+        """
         return psycopg2.connect(
             dbname="postgres",
             user=os.getenv("DB_USER"),
@@ -18,6 +30,9 @@ class DatabaseConfig:
         )
 
     def verify_table_agendamentos(self, conn):
+        """
+        Verificando se a tabela 'agendamentos' existe no banco de dados e a criando caso não exista.
+        """
         try:
             with conn.cursor() as cur:
                 cur.execute("SELECT EXISTS (SELECT FROM information_schema.tables WHERE table_name = 'agendamentos')")
